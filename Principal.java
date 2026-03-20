a3dfdc90e66b7733c14ac6cff030b2be80841f97
Arturo <arturopujante45@gamil.com>
2026-03-20 19:55:57 +0100

    Añadida fecha y hora en Principal y actualizado readme

--- Principal.java
+++ Principal.java
@@ -2,4 +2,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+public static void mostrarFechaHora() {
+        System.out.println("Fecha y hora actual: " + LocalDateTime.now());
+    }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Práctica 4
+Funcionalidad añadida: Mostrar fecha y hora actual.
+Funcionalidad añadida: Mostrar fecha y hora actual.
